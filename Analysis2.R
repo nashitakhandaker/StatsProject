e55262e6c93f0dfbc5c2f1f953c0a98b1e6bdb5c
Nashita Khandaker <nashitakhandaker@Nashitas-MacBook-Pro.local>
2026-04-09 16:48:22 -0700

    Getting SLR Kaggle score

--- Analysis2.R
+++ Analysis2.R
@@ -7,6 +7,10 @@ model_slr <- lm(SalePrice ~ GrLivArea, data = data)
 summary(model_slr)
 AIC(model_slr)
 
+# SLR Kaggle score
+test <- read.csv("test.csv")
+predictions <- predict(model_slr, testdata = test)
+
 # MLR 1
 model_mlr1 <- lm(SalePrice ~ GrLivArea + FullBath, data = data)
 summary(model_mlr1)
